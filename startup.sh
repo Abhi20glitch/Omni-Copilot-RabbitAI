ea3da792085c06fe35aa0c973e224bcdba282508
Abhi20glitch <abhilakshya04@gmail.com>
2026-04-21 20:19:33 +0530

    Create startup script for Omni Copilot to set up and start backend and frontend servers

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start the backend server
+cd backend
+npm install
+npm start &
+
+# Start the frontend server
+cd ../frontend
+npm install
+npm start
+
+# Wait for both servers to exit
+wait
